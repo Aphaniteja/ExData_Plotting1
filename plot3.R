03e85c5d78ee5b352d863153d4f1b69aa74602ef
ravenking <teja.phani@gmail.com>
2014-05-10 10:38:52 +0530

    plots 1 through 4

--- plot3.R
+++ plot3.R
@@ -14,7 +14,7 @@ p[p==6]<-"Friday"
 hpc1$weekday<-p
 hpc1$Full<-as.POSIXct(paste(hpc1$Date, hpc1$Time), format="%Y-%m-%d %H:%M:%S")
 png(filename = "plot3.png", width = 480, height = 480)
-plot(hpc1$Full,hpc[,7],type="l",ylab="Energy sub meeting",xlab=" ")
+plot(hpc1$Full,hpc[,7],type="l",ylab="Energy sub metering",xlab=" ")
 points(hpc1$Full,hpc[,8],type="l",col="red")
 points(hpc1$Full,hpc[,9],type="l",col="blue")
 legend("topright",cex=0.8, lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
